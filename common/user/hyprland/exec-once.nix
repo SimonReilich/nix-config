9a00dc49a0d0c3c5fd3c93cb5da712135e75c49e
SimonReilich <simon.reilich@proton.me>
2026-03-01 17:17:10 +0100

    Revert "fixed exec-once"
    This reverts commit df52d04cb016e706468b0ee82b264f30c0e92561.

--- common/user/hyprland/exec-once.nix
+++ common/user/hyprland/exec-once.nix
@@ -5,11 +5,8 @@
 
 {
   wayland.windowManager.hyprland.settings = {
-    exec-once = [
+    exec_once = [
       "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-      "waybar &"
-      # No need for locking right after greetd
-      # "hyprlock"
     ];
   };
 }
