b3da5dd9efa8c55e7b25bd0459c0fe16166bc719
SimonReilich <accounts@sreilich.de>
2026-07-12 19:46:57 +0200

    adapted helix config

--- computer/user/helix.nix
+++ computer/user/helix.nix
@@ -8,12 +8,6 @@
       bash-language-server
       biome
       clang-tools
-      docker-compose-language-service
-      dockerfile-language-server
-      golangci-lint
-      golangci-lint-langserver
-      gopls
-      gotools
       hongdown
       marksman
       nil
@@ -21,7 +15,6 @@
       nixpkgs-fmt
       prettier
       typescript-language-server
-      sql-formatter
       ruff
       (python3.withPackages (
         p:
@@ -37,28 +30,29 @@
       typescript
       vscode-langservers-extracted
       yaml-language-server
+      texlab
     ];
 
     themes = {
-      prompt_theme = {
+      my_theme = {
         inherits = "base16_default_dark";
         palette = {
           base00 = "#1d1f21";
           base01 = "#282a2e";
           base02 = "#373b41";
-          base03 = "#6c6c6c"; # prompt grey
+          base03 = "#6c6c6c"; 
           base04 = "#b4b7b4";
           base05 = "#c5c8c6";
           base06 = "#e0e0e0";
           base07 = "#ffffff";
-          base08 = "#3971ed"; # prompt blue for variables
-          base09 = "#fba922"; # prompt yellow for constants
-          base0A = "#fba922"; # prompt yellow for types
-          base0B = "#fba922"; # prompt yellow for strings
-          base0C = "#3971ed"; # prompt blue/cyan
-          base0D = "#3971ed"; # prompt blue for functions
-          base0E = "#a36ac7"; # prompt magenta for keywords
-          base0F = "#cc342b"; # prompt red for errors
+          base08 = "#3971ed"; 
+          base09 = "#fba922"; 
+          base0A = "#fba922"; 
+          base0B = "#fba922"; 
+          base0C = "#3971ed"; 
+          base0D = "#3971ed"; 
+          base0E = "#a36ac7"; 
+          base0F = "#cc342b"; 
         };
         "ui.background" = { };
         "ui.statusline.normal" = { fg = "base0E"; bg = "base01"; modifiers = [ "bold" ]; };
@@ -69,7 +63,7 @@
     };
 
     settings = {
-      theme = "prompt_theme";
+      theme = "my_theme";
       editor = {
         color-modes = true;
         cursorline = true;
@@ -123,122 +117,9 @@
             "file-line-ending"
             "position"
           ];
-          mode.normal = "";
-          mode.insert = "I";
-          mode.select = "S";
-        };
-      };
-
-      keys = {
-        normal = {
-          H = "extend_char_left";
-          x = "extend_to_line_bounds";
-          J = [
-            "extend_line_down"
-            "extend_to_line_bounds"
-          ];
-          K = [
-            "extend_line_up"
-            "extend_to_line_bounds"
-          ];
-          L = "extend_char_right";
-          B = "extend_prev_word_start";
-          W = "extend_next_word_start";
-          E = "extend_next_word_end";
-          A-b = "move_prev_long_word_start";
-          A-B = "extend_prev_long_word_start";
-          A-w = "move_next_long_word_start";
-          A-W = "extend_next_long_word_start";
-          A-e = "move_next_long_word_end";
-          A-E = "extend_next_long_word_end";
-          T = "extend_till_char";
-          F = "extend_next_char";
-          A-t = "till_prev_char";
-          A-f = "find_prev_char";
-          A-T = "extend_till_prev_char";
-          A-F = "extend_prev_char";
-          A-j = "join_selections";
-          A-k = "keep_selections";
-          M = [
-            "select_mode"
-            "match_brackets"
-            "normal_mode"
-          ];
-          "#" = "toggle_comments";
-          A-l = "extend_to_line_end";
-          A-h = "extend_to_line_start";
-          left = "goto_previous_buffer";
-          right = "goto_next_buffer";
-          A-d = "delete_selection";
-          c = "change_selection_noyank";
-          d = "delete_selection_noyank";
-          N = "extend_search_next";
-          A-n = "search_prev";
-          A-N = "extend_search_prev";
-          C-d = [
-            "page_cursor_half_down"
-            "align_view_center"
-          ];
-          C-u = [
-            "page_cursor_half_up"
-            "align_view_center"
-          ];
-          C-j = [
-            "extend_to_line_bounds"
-            "delete_selection"
-            "paste_after"
-          ];
-          C-k = [
-            "extend_to_line_bounds"
-            "delete_selection"
-            "move_line_up"
-            "paste_before"
-          ];
-          tab = "move_parent_node_end";
-          S-tab = "move_parent_node_start";
-
-          G = {
-            j = "@vgj<esc>";
-            k = "@vgk<esc>";
-          };
-
-          g = {
-            j = "goto_last_line";
-            k = "goto_file_start";
-          };
-
-          space = {
-            c = ":buffer-close";
-            A-f = ":toggle auto-format";
-            q = ":write-quit-all";
-            Q = ":quit!";
-            w = ":write";
-            "." = ":toggle file-picker.git-ignore";
-          };
-        };
-
-        insert = {
-          C-u = [
-            "extend_to_line_bounds"
-            "delete_selection_noyank"
-            "open_above"
-          ];
-          C-w = [
-            "move_prev_word_start"
-            "delete_selection_noyank"
-          ];
-          C-space = "completion";
-          S-tab = "move_parent_node_start";
-        };
-
-        select = {
-          tab = "extend_parent_node_end";
-          S-tab = "extend_parent_node_start";
-
-          g = {
-            j = "goto_last_line";
-            k = "goto_file_start";
-          };
+          mode.normal = "󰆾";
+          mode.insert = "󰏫";
+          mode.select = "󰒉";
         };
       };
     };
@@ -253,12 +134,26 @@
         command = "clippy";
       };
 
-      language-server.yaml-language-server.config.yaml.schemas = {
-        kubernetes = "k8s/*.yaml";
-      };
-
       language-server.typescript-language-server.config.tsserver = {
         path = "${pkgs.typescript}/lib/node_modules/typescript/lib/tsserver.js";
+      };
+
+      language-server.texlab = {
+        config = {
+          latex = {
+            build = {
+              onSave = true;
+              executable = "latexmk";
+              args = [ 
+                "-lualatex"
+                "-shell-escape" 
+                "-interaction=nonstopmode"
+                "-synctex=1"
+                "%f" 
+              ];
+            };
+          };
+        };
       };
 
       language = [
@@ -271,17 +166,7 @@
           ];
           auto-format = true;
         }
-        {
-          name = "go";
-          language-servers = [
-            "gopls"
-            "golangci-lint-lsp"
-          ];
-          formatter = {
-            command = "goimports";
-          };
-          auto-format = true;
-        }
+
         {
           name = "html";
           language-servers = [
@@ -297,6 +182,7 @@
           };
           auto-format = true;
         }
+        
         {
           name = "javascript";
           language-servers = [
@@ -309,6 +195,7 @@
           ];
           auto-format = true;
         }
+        
         {
           name = "json";
           language-servers = [
@@ -330,54 +217,7 @@
           };
           auto-format = true;
         }
-        {
-          name = "jsonc";
-          language-servers = [
-            {
-              name = "vscode-json-language-server";
-              except-features = [ "format" ];
-            }
-            "biome"
-          ];
-          formatter = {
-            command = "biome";
-            args = [
-              "format"
-              "--indent-style"
-              "space"
-              "--stdin-file-path"
-              "%{buffer_name}"
-            ];
-          };
-          file-types = [
-            "jsonc"
-            "hujson"
-          ];
-          auto-format = true;
-        }
-        {
-          name = "jsx";
-          language-servers = [
-            {
-              name = "typescript-language-server";
-              except-features = [ "format" ];
-            }
-            "tailwindcss-ls"
-            "biome"
 
-          ];
-          formatter = {
-            command = "biome";
-            args = [
-              "format"
-              "--indent-style"
-              "space"
-              "--stdin-file-path"
-              "%{buffer_name}"
-            ];
-          };
-          auto-format = true;
-        }
         {
           name = "markdown";
           language-servers = [ "marksman" ];
@@ -390,6 +230,7 @@
           };
           auto-format = true;
         }
+        
         {
           name = "nix";
           formatter = {
@@ -397,6 +238,7 @@
           };
           auto-format = true;
         }
+        
         {
           name = "python";
           language-servers = [ "pylsp" ];
@@ -409,24 +251,13 @@
           };
           auto-format = true;
         }
+        
         {
           name = "rust";
           language-servers = [ "rust-analyzer" ];
           auto-format = true;
         }
-        {
-          name = "sql";
-          formatter = {
-            command = "sql-formatter";
-            args = [
-              "-l"
-              "postgresql"
-              "-c"
-              "{\"keywordCase\": \"lower\", \"dataTypeCase\": \"lower\", \"functionCase\": \"lower\", \"expressionWidth\": 120, \"tabWidth\": 4}"
-            ];
-          };
-          auto-format = true;
-        }
+
         {
           name = "toml";
           language-servers = [ "taplo" ];
@@ -441,29 +272,7 @@
           };
           auto-format = true;
         }
-        {
-          name = "tsx";
-          language-servers = [
-            {
-              name = "typescript-language-server";
-              except-features = [ "format" ];
-            }
-            "tailwindcss-ls"
-            "biome"
 
-          ];
-          formatter = {
-            command = "biome";
-            args = [
-              "format"
-              "--indent-style"
-              "space"
-              "--stdin-file-path"
-              "%{buffer_name}"
-            ];
-          };
-          auto-format = true;
-        }
         {
           name = "typescript";
           language-servers = [
@@ -486,6 +295,7 @@
           };
           auto-format = true;
         }
+        
         {
           name = "yaml";
           language-servers = [ "yaml-language-server" ];
@@ -497,6 +307,11 @@
             ];
           };
           auto-format = true;
+        }
+
+        {
+          name = "latex";
+          language-servers = [ "texlab" ];
         }
       ];
     };
